326fe00c60fa563bf58d480e3283fa35069372c6
raphael.kapeller <raphael.kapeller@sz-ybbs.ac.at>
2022-10-14 10:41:51 +0200

    changes

--- CarApi/Models/Car.cs
+++ CarApi/Models/Car.cs
@@ -5,7 +5,7 @@ public class Car
     public int Id { get; set; }
     private string Manufacturer { get; set; }
     private string Type { get; set; }
-    private string Salesprice { get; set; }
+    private double Salesprice { get; set; }
     private DateTime NextCheck { get; set; }
     private int Power { get; set; }
     private int Km { get; set; }
